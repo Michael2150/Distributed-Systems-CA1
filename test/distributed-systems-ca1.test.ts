7d1f2903c7fa711bf5626c609f795d3633107cbd
Michael Gerber <michaelgerb2150@gmail.com>
2023-11-07 20:14:39 +0000

    Project Init

---
+++ test/distributed-systems-ca1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistributedSystemsCa1 from '../lib/distributed-systems-ca1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/distributed-systems-ca1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistributedSystemsCa1.DistributedSystemsCa1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
