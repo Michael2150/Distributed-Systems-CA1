0c4ec4d106d07df1a3dc78b377ffa7022cd4c24c
Michael Gerber <michaelgerb2150@gmail.com>
2023-11-12 14:54:41 +0000

    Create DynamoDB Tables

--- lib/distributed-systems-ca1-stack.ts
+++ lib/distributed-systems-ca1-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import {Movie, Review} from '../shared/types';
 
 export class DistributedSystemsCa1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    // DynamoDB tables for movies and reviews
+    const movieTable = new dynamodb.Table(this, 'MovieTable', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: 'Movies',
+    });
 
-    // The code that defines your stack goes here
+    const reviewTable = new dynamodb.Table(this, 'ReviewTable', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: 'Reviews',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DistributedSystemsCa1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Add a global secondary index to the review table
+    reviewTable.addGlobalSecondaryIndex({
+      indexName: 'movie_id-index',
+      partitionKey: { name: 'movie_id', type: dynamodb.AttributeType.NUMBER },
+    });
+
+    // Fill the tables with data
+    const movieData = require('../shared/movies.json');
+    const reviewData = require('../shared/reviews.json');
+    
+
   }
 }
--- test/distributed-systems-ca1.test.ts
+++ test/distributed-systems-ca1.test.ts
@@ -1,7 +1,3 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as DistributedSystemsCa1 from '../lib/distributed-systems-ca1-stack';
-
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/distributed-systems-ca1-stack.ts
 test('SQS Queue Created', () => {
